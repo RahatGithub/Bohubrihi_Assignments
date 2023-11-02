90f15baa19b721824198b6b594b83695d4a6cb26
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2023-11-02 14:48:09 +0600

    Made case insensitive

--- Py_Word_Counter/program.py
+++ Py_Word_Counter/program.py
@@ -1,9 +1,9 @@
 searchWords = ["PHP", "Java", "Python", "C", "C++"]
 
-with open('Word_Counter_Py\input.txt', 'r') as file:
+with open('Py_Word_Counter\input.txt', 'r') as file:
     lines = file.readlines() 
-    lines = [line.strip() for line in lines]
+    lines = [line.strip().capitalize() for line in lines]
 
 for word in searchWords:
-    count = lines.count(word)
+    count = lines.count(word.capitalize())
     print(f"{word}->{count}")
