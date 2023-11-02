4f7ab7af34cf0bc2f80fda1a4771900db42dfd11
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2023-11-02 14:56:24 +0600

    similar search words removed

--- Py_Word_Counter/program.py
+++ Py_Word_Counter/program.py
@@ -1,9 +1,12 @@
-searchWords = ["PHP", "Java", "Python", "C", "C++"]
+searchWords = ["PHP", "java", "Python", "C", "c++", "python"]
 
 with open('Py_Word_Counter\input.txt', 'r') as file:
     lines = file.readlines() 
-    lines = [line.strip().capitalize() for line in lines]
+    lines = [line.strip().upper() for line in lines]
+
+searchWords = set(map(lambda s: s.upper(), searchWords))
 
 for word in searchWords:
-    count = lines.count(word.capitalize())
+    word = word.upper()
+    count = lines.count(word)
     print(f"{word}->{count}")
